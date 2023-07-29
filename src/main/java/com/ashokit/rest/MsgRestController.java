a9a1dcce99e67faf1e982e598eb03db3810ba483
venkat4-sys <118825230+venkat4-sys@users.noreply.github.com>
2023-07-29 15:08:03 +0530

    Update MsgRestController.java

--- src/main/java/com/ashokit/rest/MsgRestController.java
+++ src/main/java/com/ashokit/rest/MsgRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MsgRestController {
 	
-	@GetMapping("/msg")
+	@GetMapping
 	public String getMsg() {
 		
 		return "Welcome To Ashok IT";
